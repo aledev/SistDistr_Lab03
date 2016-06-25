e99586cd099b46b12c204ecf3db336bf86a75b4f
Ana Karina Villagran <akarinavillagrani@gmail.com>
2016-06-25 20:06:14 -0300

    add phyton client

---
+++ Clientes/Cliente_Python/Cliente.py
@@ -0,0 +1,28 @@
+import requests 
+import json
+
+def consumeGet():
+    url_json = 'http://localhost:8080/usuario/list'
+    res = requests.get(dest)
+    body = res.text 
+    print(boby)
+
+def consumePost():
+    url_json = 'https://github.com/timeline.json'
+    res = requests.get(dest)
+    body = res.text 
+    print(boby)
+
+def consumeDelete():
+    url_json = 'https://github.com/timeline.json'
+    res = requests.get(dest)
+    body = res.text 
+    print(boby)
+
+def consumePut():
+    url_json = 'https://github.com/timeline.json'
+    res = requests.get(dest)
+    body = res.text 
+    print(boby)
+
+consumeGet()
